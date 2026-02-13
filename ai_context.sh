b83039a22ffa48b29a17e083962dea7013d06c9d
Joseph-Alpha <2293216510@qq.com>
2026-02-13 17:23:26 +0800

    修改了脚本，以使其能够获取 8. 核心模块.md ，以后这种就直接上传了吧

--- ai_context.sh
+++ ai_context.sh
@@ -3,3 +3,6 @@ set -eu
 
 export OBSIDIAN_CONTRACTS_DIR="/home/ysf/文档/笔记仓库/阿尔法/🌊 001 RIVER · 河流/013 Projects 项目/阿尔法/"
 python3 ./scripts/gen_ai_context.py
+
+#保存测试命令
+#python -m unittest tests/test_gen_ai_context.py
